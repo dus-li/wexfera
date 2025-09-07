94d923b4e114094b7693fe85d1692c78c764ea3d
Duszku <duszku511@gmail.com>
2025-09-07 19:36:51 +0200

    ref/compiler: Sort attribute macros
    Before they were sort of scattered. Now parametrized and
    non-parametrized defines are separated and proper comments make the file
    cleaner and more readable.
    
    Signed-off-by: Duszku <duszku511@gmail.com>

--- include/lib/compiler.h
+++ include/lib/compiler.h
@@ -8,18 +8,22 @@
  * @brief Compiler builtins and attributes and pseudo-keywords.
  */
 
+// Non-parametrized attributes
+#define __closed __attribute__((enum_extensibility(closed)))
+#define __naked  __attribute__((naked))
+#define __retain __attribute__((retain))
+#define __used   __attribute__((used))
+
+// Parametrized attributes
 #define __cleanup(_func)        __attribute__((__cleanup__(_func)))
-#define __closed                __attribute__((enum_extensibility(closed)))
-#define __naked                 __attribute__((naked))
 #define __printfmt(_fmt, _args) __attribute__((format(printf, (_fmt), (_args))))
-#define __retain                __attribute__((retain))
 #define __section(_sec)         __attribute__((section(_sec)))
-#define __used                  __attribute__((used))
 
 #define ctz(_val) __builtin_ctz(_val)
 
 #define same_type(_fst, _snd) \
 	__builtin_types_compatible_p(typeof(_fst), typeof(_snd))
 
+// Pseudo-keywords
 #define fallthrough __attribute__((fallthrough))
 #define loop        for (;;)
